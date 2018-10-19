0401943e9f7e729919ab229cd52a458834c1cecf
Jakub Dyszkiewicz <jakub.dyszkiewicz@allegro.pl>
2018-10-19 11:10:48 +0200

    Simple UI that refreshes Raft Node

--- rafting-dashboard/src/components/HelloWorld.vue
+++ rafting-dashboard/src/components/HelloWorld.vue
@@ -1,95 +1,97 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+
+    <input v-model="peerAddress" title="peer address"/>
+    <button v-on:click="addPeer()">Add peer</button>
+
+    <div v-for="peer in peers" :key="peer.address">
+        <p class="peers">{{peer.address}}</p>
+        <button class="peers" v-on:click="deletePeer(peer)">X</button>
+        <table v-if="states[peer.address]">
+          <tr>
+            <td>Node ID: </td>
+            <td>{{ states[peer.address].nodeId}}</td>
+          </tr>
+          <tr>
+            <td>Role: </td>
+            <td>{{ states[peer.address].role}}</td>
+          </tr>
+          <tr>
+            <td>Current Term: </td>
+            <td>{{ states[peer.address].currentTerm }}</td>
+          </tr>
+          <tr>
+            <td>Last heartbeat: </td>
+            <td>{{ states[peer.address].lastHeartbeat }}</td>
+          </tr>
+          <tr>
+            <td>Peers: </td>
+            <td>{{ states[peer.address].peers}}</td>
+          </tr>
+          <tr>
+            <td>Voted for: </td>
+            <td>{{ states[peer.address].votedFor}}</td>
+          </tr>
+          <tr>
+            <td>Votes granted: </td>
+            <td>{{ states[peer.address].votesGranted}}</td>
+          </tr>
+        </table>
+        <p v-else>No data yet</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Rafting Dashboard',
+      peerAddress: '',
+      peers: [
+        {
+          id: 123,
+          address: '172.18.200.240:5000'
+        }
+      ],
+      states: {}
     }
+  },
+  computed: {
+    statesVO: function () {
+      return Object.entries(this.states)
+    }
+  },
+  methods: {
+    addPeer: function () {
+      this.peers.push({address: this.peerAddress})
+      this.peerAddress = ''
+    },
+    deletePeer: function (peerToDelete) {
+      this.peers = this.peers.filter(peer => peer !== peerToDelete)
+    },
+    refreshState: function () {
+      this.peers.forEach(peer => {
+        axios.get(`http://${peer.address}/raft/state`)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            this.states[peer.address] = response.data
+            this.$forceUpdate()
+            console.log(response.data)
+            console.log(Object.entries(this.states))
+          })
+          .catch(e => {
+            console.error(e)
+          })
+      })
+    }
+  },
+  created () {
+    setInterval(() => this.refreshState(), 5000)
   }
 }
 </script>
@@ -109,5 +111,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.peers {
+  display: inline-block;
+}
+.nodes {
+  display: inline-block;
 }
 </style>
